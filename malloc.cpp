e1b7cbf6cae4b5656352c03b8cca9d6752fd5ceb
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2024-02-12 17:32:58 +0530

    removed unneccesary complexity

--- malloc.cpp
+++ malloc.cpp
@@ -4,59 +4,39 @@
 #include <cstdint>
 #include <sys/mman.h>
 
-std::array<head*, POOL_ARRAY_SIZE>      poolArrayLevel1;  // 4KB blocks
-std::array<head*, POOL_ARRAY_SIZE * 2>  poolArrayLevel2;  // 2KB blocks
-std::array<head*, POOL_ARRAY_SIZE * 4>  poolArrayLevel3;  // 1KB blocks
-std::array<head*, POOL_ARRAY_SIZE * 8>  poolArrayLevel4;  // 512B blocks
-std::array<head*, POOL_ARRAY_SIZE * 16> poolArrayLevel5;  // 128B blocks
-std::array<head*, POOL_ARRAY_SIZE * 32> poolArrayLevel6;  // 64B blocks
-std::array<head*, POOL_ARRAY_SIZE * 64> poolArrayLevel7;  // 32B blocks
+head* freeList = nullptr;
 
-std::array<head*, LEVELS> poolArrays;
-  // poolArrayLevel2 = *reinterpret_cast<std::array<head*, POOL_ARRAY_SIZE * 2>*>(memoryPool);
-    // poolArrayLevel3 = *reinterpret_cast<std::array<head*, POOL_ARRAY_SIZE * 4>*>(memoryPool);
-    // poolArrayLevel4 = *reinterpret_cast<std::array<head*, POOL_ARRAY_SIZE * 8>*>(memoryPool);
-    // poolArrayLevel5 = *reinterpret_cast<std::array<head*, POOL_ARRAY_SIZE * 16>*>(memoryPool);
-    // poolArrayLevel6 = *reinterpret_cast<std::array<head*, POOL_ARRAY_SIZE * 32>*>(memoryPool);
-    // poolArrayLevel7 = *reinterpret_cast<std::array<head*, POOL_ARRAY_SIZE * 64>*>(memoryPool);
 void init(){
    // put poolArrayLevel1 on the mmap'd memory
    // write a function to divide the chunks in case of a memory allocation
-    //
-    void* memoryPool = malloc(100);
-    poolArrayLevel1 = *new(memoryPool) std::array<head*, POOL_ARRAY_SIZE>();
-    poolArrays[0] = reinterpret_cast<head*>(poolArrayLevel1.data());
-    poolArrays[1] = reinterpret_cast<head*>(poolArrayLevel2.data());
-    poolArrays[2] = reinterpret_cast<head*>(poolArrayLevel3.data());
-    poolArrays[3] = reinterpret_cast<head*>(poolArrayLevel4.data());
-    poolArrays[4] = reinterpret_cast<head*>(poolArrayLevel5.data());
-    poolArrays[5] = reinterpret_cast<head*>(poolArrayLevel6.data());
-    poolArrays[6] = reinterpret_cast<head*>(poolArrayLevel7.data());
 
-
-    int numBlocks = 12*1024*1024 / 4096; // 3072
-    // Allocate 12MB block
     void* memory = mmap(nullptr, 12*1024*1024, PROT_READ|PROT_WRITE,
                         MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-    // Pointer to first block
-    head* current = static_cast<head*>(memory);
-    head* rootHead = current;
+    if (memory == MAP_FAILED) {
+        return;
+    }
+    freeList = (head*)memory;
+    head* currentNode = freeList;
 
-    // Link all blocks
-    for (int i = 0; i < numBlocks-1; i++) {
-
-        // Current block
-        current->next =
-               reinterpret_cast<head*>(reinterpret_cast<uintptr_t>(current) + 4096);
-        if (i != 0){
-            current->previous = reinterpret_cast<head*>(reinterpret_cast<uintptr_t>(current) - 4096);
+    for (int i = 0; i < (sizes[i] - 1); i++) {
+        if (i != 0) {
+            currentNode->previous = (head*)((uintptr_t)currentNode - PAGE);
         }
-        // Advance to next block
-        current = current->next;
+        else {
+            currentNode->previous = nullptr;
+        }
+
+        currentNode->next = (head*)((uintptr_t)currentNode + PAGE);
+        currentNode->left = currentNode->right = nullptr;
+        currentNode->level = 0;
+        currentNode->status = unused;
+        currentNode = currentNode->next;
     }
 
-    // Terminate list
-    current->next = nullptr;
+    test();
+}
 
-
+void test() {
+    std::cout << "Test" << std::endl;
+    // poolArrayLevel1[0]->left = nullptr;
 }
--- malloc.hpp
+++ malloc.hpp
@@ -30,22 +30,9 @@ struct head{
 };
 
 void init();
+void test();
 
 extern std::array<head*, LEVELS> poolArrays;
-// extern head* poolArrayLevel1[];
-// extern head* poolArrayLevel2[];
-// extern head* poolArrayLevel3[];
-// extern head* poolArrayLevel4[];
-// extern head* poolArrayLevel5[];
-// extern head* poolArrayLevel6[];
-// extern head* poolArrayLevel7[];
-
 extern std::array<head*, POOL_ARRAY_SIZE> poolArrayLevel1;      // 4KB blocks
-extern std::array<head*, POOL_ARRAY_SIZE * 2> poolArrayLevel2;  // 2KB blocks
-extern std::array<head*, POOL_ARRAY_SIZE * 4> poolArrayLevel3;  // 1KB blocks
-extern std::array<head*, POOL_ARRAY_SIZE * 8> poolArrayLevel4;  // 512B blocks
-extern std::array<head*, POOL_ARRAY_SIZE * 16> poolArrayLevel5; // 128B blocks
-extern std::array<head*, POOL_ARRAY_SIZE * 32> poolArrayLevel6; // 64B blocks
-extern std::array<head*, POOL_ARRAY_SIZE * 64> poolArrayLevel7; // 32B blocks
 
 #endif
